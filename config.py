c874fcddeeff72967dc08628af7e85348a1bd793
Harry McGhee <harry_mcghee@hotmail.com>
2023-11-28 09:27:40 +0000

    Added save to pdf functionality

--- config.py
+++ config.py
@@ -5,60 +5,86 @@ from lmfit import Model
 from lmfit.models import GaussianModel
 from lmfit import Parameters
 import config
-from functions import smooth
+from functions import smooth, prepare_data
 import os
 
 # import data matrix
 
 data_path = 'test_data/para_matrix.txt'
 
-matrix = np.genfromtxt(data_path)
+# set smoothing window
 
-delay = matrix[0,1:]
-energy = matrix[1:,0]
-z = matrix[1:,1:]
+smooth = 3
 
-smooth = 5
+# save pdf?
 
-#Define model
+save = True
+outfile = 'output_files/test.pdf'
 
-gs = np.genfromtxt('/Users/harrymcghee/Dropbox (UCL)/4_PROJECTS/5_PALRIXS/3_BEAMTIME/2_SCRIPTS/6_POST/TXT/pNA_gs.txt')
+# get energy axis to interpolate basis functions (leave this)
+energy = prepare_data(data_path,smooth)[1]
+
+#add any basis functions here
+
+gs = np.genfromtxt('test_data/pNA_gs_calib.txt')
 gs = np.interp(energy,gs[:,0],gs[:,1])
 
+aniline = np.genfromtxt('/Users/harrymcghee/Dropbox (UCL)/4_PROJECTS/5_PALRIXS/3_BEAMTIME/2_SCRIPTS/6_POST/lit_spectra/aniline.csv',delimiter = ',')
+aniline = np.interp(energy,aniline[:,0],aniline[:,1])
 
-def bkg(x,c):
-    return c*gs[np.where(x==x)]
+NO = np.genfromtxt('/Users/harrymcghee/Dropbox (UCL)/4_PROJECTS/5_PALRIXS/3_BEAMTIME/2_SCRIPTS/6_POST/lit_spectra/NO.csv',delimiter = ',')
+NO = np.interp(energy,NO[:,0],NO[:,1])
 
-model = GaussianModel(prefix = 'N2_')+ GaussianModel(prefix = 'g1_') + GaussianModel(prefix = 'g2_') + GaussianModel(prefix = 'g3_') + GaussianModel(prefix = 'g4_')- Model(bkg)
+NO2 = np.genfromtxt('/Users/harrymcghee/Dropbox (UCL)/4_PROJECTS/5_PALRIXS/3_BEAMTIME/2_SCRIPTS/6_POST/lit_spectra/NO2.csv',delimiter = ',')
+NO2 = np.interp(energy,NO2[:,0],NO2[:,1])
 
+
+def gs_fit(x,amplitude):
+    return amplitude*gs[np.where(x==x)]
+
+def aniline_fit(x,amplitude):
+    return amplitude*aniline[np.where(x==x)]
+
+def NO_fit(x,amplitude):
+    return amplitude*NO[np.where(x==x)]
+
+def NO2_fit(x,amplitude):
+    return amplitude*NO2[np.where(x==x)]
+
+
+# model = GaussianModel(prefix = 'g1_') + GaussianModel(prefix = 'g2_')  + GaussianModel(prefix = 'g4_') - Model(bkg,prefix = 'gs_')
+# model = GaussianModel(prefix = 'g1_') + GaussianModel(prefix = 'g2_') +GaussianModel(prefix = 'g3_') - Model(bkg,prefix = 'gs_')
+
+model =  Model(NO_fit,prefix = 'NO_') + Model(NO2_fit,prefix = 'NO2_') - Model(gs_fit,prefix = 'gs_')
 
 #set initial parameters
 
 params = Parameters()
 
-
-params.add('N2_amplitude', value=0.4, min = 0, vary = False)
-params.add('N2_center', value=429.07,vary = False)
-params.add('N2_sigma', value=0.4,max = 1,vary = False)
-
-params.add('g1_amplitude', value=0.7, min = 0,vary = True)
-params.add('g1_center', value=429.07,vary = False)
-params.add('g1_sigma', value=0.4,max = 1,vary = True)
-
-params.add('g2_amplitude', value=0.25,min = 0)
-params.add('g2_center', value=430)
-params.add('g2_sigma', value=0.6,max = 1,vary = True)
-
-params.add('g3_amplitude', value=0.25,min = 0,)
-params.add('g3_center', value=433.5)
-params.add('g3_sigma', value=0.6,max = 1,vary = True)
-
-params.add('g4_amplitude', value=0.25,min = 0)
-params.add('g4_center', value=427.5, vary = False)
-params.add('g4_sigma', value=0.4,max = 1,vary = False)
+params.add('gs_amplitude', value=0.1,vary = True)
+# params.add('aniline_amplitude', value=0.1,vary = True)
+params.add('NO_amplitude', value=0.1,vary = True)
+params.add('NO2_amplitude', value=0.1,vary = True)
 
 
-params.add('c', value=1,vary = False)
+# params.add('gs_amplitude', value=0.1,vary = True)
+
+# params.add('g1_amplitude', value=0.025,min = 0)
+# params.add('g1_center', value=399.65, vary = False)
+# params.add('g1_sigma', value=0.4,min = 0.2,max = 0.6,vary = True)
+
+# params.add('g2_amplitude', value=0.07, min = 0,vary = True)
+# params.add('g2_center', value=400.65,vary = False)
+# params.add('g2_sigma', value=0.4,min = 0.2,max = 0.6,vary = True)
+
+# params.add('g3_amplitude', value = 0.1, min = 0,vary = True)
+# params.add('g3_center', value=403,vary = False)
+# params.add('g3_sigma', value=0.4,min = 0.2,max = 0.6,vary = True)
+
+# params.add('g4_amplitude', expr = 'g1_amplitude', min = 0,vary = True)
+# params.add('g4_center', value=405,vary = False)
+# params.add('g4_sigma', value=0.4,max = 1,expr = 'g1_sigma',vary = True)
+
 
 param_names = model.param_names
 param_names = [name.split('_')[0] for name in param_names]
--- fit.py
+++ fit.py
@@ -1,106 +1,135 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import matplotlib.backends.backend_pdf
+
 
 from lmfit import Model
 from lmfit.models import GaussianModel
 from lmfit import Parameters
-import config
-from functions import smooth
+import importlib
+from functions import smooth, prepare_data
 import os
 import matplotlib.gridspec as gridspec
 
-plt.style.use('style')
+#reload config file so kernal doesn't have to be restarted 
+config_file = 'config' #make ths sysargv?
+config = importlib.import_module(config_file)
+config = importlib.reload(config)
 
+#set plot style
+
+plt.style.use('style') 
 
 #Import data from config file
 
-matrix = np.genfromtxt(config.data_path)
+delay,energy,z = prepare_data(config.data_path,config.smooth)
 
-delay = matrix[0,1:]
-energy = matrix[1:,0]
-z = matrix[1:,1:]
-
-#Apply any smoothing or truncation
-
-z = [smooth(x,config.smooth) for x in z]
-z = np.array(z)
-
-#Plot heat map
-
-plt.pcolormesh(delay,energy,z)
-plt.xscale('symlog')
-plt.xlabel('Delay (ps)')
-plt.ylabel('Energy (eV)')
-
-#import model from config file
+#Import model from config file
 
 model = config.model
 params = config.params
 param_names = config.param_names
 
 #plot
-fit_matrix = []
 
+
+fit_matrix = []
 vars_dict = {}
 for name in param_names:
     vars_dict[name] = []
 
-
-gs = gridspec.GridSpec(math.ceil(len(delay)/2), 2)
-plt.figure(figsize=(10,len(delay)*2))
+if config.save:
+    pdf = matplotlib.backends.backend_pdf.PdfPages(config.outfile)
 
 for idx in range(0,len(delay)):
 
     y = z[:,idx]
-    y = y/np.max(abs(y))
 
     result = model.fit(y, params, x=energy)
     comps = result.eval_components(x=energy)
+    fig = plt.figure(figsize = (3,2))
 
-    # Create a new subplot on the grid
-    ax = plt.subplot(gs[idx // 2, idx % 2])
-
-    # Plot the data on the subplot
-    ax.plot(energy,y,'o',markersize = 2)
-    ax.plot(energy,result.best_fit,color = 'red')
-    ax.set_title('{} ps'.format(delay[idx]))
+    plt.plot(energy,y,'o',markersize = 2)
+    plt.plot(energy,result.best_fit,color = 'red')
+    plt.title('{} ps'.format(delay[idx]))
     for i in comps:
-        ax.fill_between(energy,0,comps[i],alpha = 0.2,label = i)
+        plt.fill_between(energy,0,comps[i],alpha = 0.2,label = i)
     
     fit_matrix.append(result.best_fit)
 
     for name in param_names:
-        if name == 'c':
-            vars_dict[name].append(result.params[name].value)
-        else:
-            vars_dict[name].append(result.params[name+'_amplitude'].value)
+        vars_dict[name].append(result.params[name+'_amplitude'].value)
     
-    ax.set_xlabel('Energy (eV)')
-    ax.set_ylabel('Intensity (a.u.)')
-    ax.legend()
+    plt.xlabel('Energy (eV)')
+    plt.ylabel('Intensity (a.u.)')
+    plt.legend(ncol = 2,loc = 'lower left')
+    plt.ylim(-0.05,0.05)
+    plt.tight_layout()
 
-# Show the plot
+    if config.save:
+        pdf.savefig(fig)
+
+fit_matrix = np.array(fit_matrix).T
 plt.tight_layout()
 plt.show()
 
-plt.figure()
-plt.pcolormesh(delay,energy,np.array(fit_matrix).T)
-plt.xscale('symlog')
-plt.xlabel('Delay (ps)')
-plt.ylabel('Energy (eV)')
-plt.show()
 
-plt.figure(figsize = (10,15))
-gs = gridspec.GridSpec(6, 2)
+#colormaps
 
-for j,i in enumerate([N2,g1,g2,g3,g4,c]):
+titles = ['raw data','fitted data','residual']  
+for l,i in enumerate([z,fit_matrix,z-fit_matrix]):
+    fig = plt.figure(figsize = (3,2))
+    plt.pcolormesh(delay,energy,i)
+    plt.colorbar()
+    plt.xlabel('Delay / ps')
+    plt.ylabel('Energy / eV')
+    plt.title(titles[l])
+    plt.show()
+    if config.save:
+        pdf.savefig(fig)
+
+
+
+# fig,(ax1,ax2,ax3) = plt.subplots(3,1,figsize = (6,10))
+
+# ax1.pcolormesh(delay,energy,z)
+# ax1.set_title('raw data')
+
+# ax2.pcolormesh(delay,energy,np.array(fit_matrix).T)
+# ax2.set_title('fitted data')
+
+# ax3.pcolormesh(delay,energy,z-np.array(fit_matrix).T)
+# ax3.set_title('residual')
+
+# for i in [ax1,ax2,ax3]:
+#     # i.set_xscale('symlog')
+#     i.set_xlabel('Delay / ps')
+#     i.set_ylabel('Energy / eV')
+# plt.tight_layout()
+# plt.show()
+
+# Plot kinetic components
+fig = plt.figure(figsize = (3,len(param_names)*2))
+gs = gridspec.GridSpec(len(param_names), 1)
+
+for j,i in enumerate(param_names):
     ax = plt.subplot(gs[j, :])
-    ax.plot(delay,i,'-x',markersize = 2,label = param_names[j])
+    ax.plot(delay,vars_dict[i],'-x',label = i)
     ax.set_xscale('symlog',linthresh = 10)
     ax.set_xlabel('Delay (ps)')
     ax.set_ylabel('Amplitude (a.u.)')
     ax.legend()
-
+    
 plt.tight_layout()
+if config.save:
+    pdf.savefig(fig)
+    pdf.close()
+
+#check no kinetic behaviour of widths / centres
+#save out to test fits
+
+# x = delay
+# y = vars_dict['g2']
+# np.savetxt('g2.txt',np.c_[x,y])
 
